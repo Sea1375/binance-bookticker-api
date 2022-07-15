7e87749950c3ee1fddee4ad02f5dfff48272a488
Sea1375 <sea.dream0000@gmail.com>
2022-07-15 06:28:03 -0700

    bookticker

---
+++ web_socket_bookticker.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'php-binance-api.php';
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$bucket = "order-ticket-api";
+                        
+$s3 = new S3Client([
+    "region"  => "ap-northeast-1"
+]);
+
+// @see home_directory_config.php
+// use config from ~/.confg/jaggedsoft/php-binance-api.json
+$api = new Binance\API("PSxGmJaZYnZabIx1c66peOV2vucR6jLuOQVIpoV1jMjPU0yGLMRey2V0t5QyhDrn", "UD0jnVLSBRnKqIcBHg1d6trC5PKqsu7DNdNKcXHrR9U6lKXLEbt6Gj78UHfFKA6A");
+
+// Ticker Updates via WebSocket
+$api->bookTicker(function($api, $ticker) {
+  // print_r($ticker);
+  $keyname = "ticker";
+
+  try {
+    // Upload data.
+    $result = $s3->putObject([
+        "Bucket" => $bucket,
+        "Key"    => $keyname,
+        "Body"   => $ticker,
+        "ACL"    => "public-read"
+    ]);
+
+    // Print the URL to the object.
+    echo $result['ObjectURL'] . PHP_EOL;
+  } catch (S3Exception $e) {
+      echo $e->getMessage() . PHP_EOL;
+  }
+});
